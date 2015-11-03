92def6c3eb51d9e862f6e5460c4946edf89cbde3
Mark Rivers <rivers@cars.uchicago.edu>
2015-11-02 18:27:55 -0600

    New file

---
+++ iocBoot/iocUSBC9513/st.cmd
@@ -0,0 +1,13 @@
+## Configure port driver
+# USB4303Config(portName,        # The name to give to this asyn port driver
+#               boardNum,        # The number of this board assigned by the Measurement Computing Instacal program 
+#               numChips)        # Number of CTS9513 chips on this module
+C9513Config("C9513_1", 0, 2)
+dbLoadTemplate("C9513.substitutions")
+
+< save_restore.cmd
+
+iocInit
+
+create_monitor_set("auto_settings.req",30)
+
