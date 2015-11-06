99da183b7ae4c23d3ea7e91162af426a72f51e28
Mark Rivers <rivers@cars.uchicago.edu>
2015-11-06 07:07:43 -0600

    Add additional commands at start to make this file standalone

--- iocBoot/iocUSBC9513/st.cmd
+++ iocBoot/iocUSBC9513/st.cmd
@@ -1,3 +1,9 @@
+< envPaths
+
+## Register all support components
+dbLoadDatabase "../../dbd/measCompApp.dbd"
+measCompApp_registerRecordDeviceDriver pdbbase
+
 ## Configure port driver
 # USB4303Config(portName,        # The name to give to this asyn port driver
 #               boardNum,        # The number of this board assigned by the Measurement Computing Instacal program 
