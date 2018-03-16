ed066d7730a9d8eacbba74511f12328935364ee0
Mark Rivers <rivers@cars.uchicago.edu>
2018-03-16 08:29:55 -0500

    Got simple analog input working; add aInScan, aLoadQueue

--- measCompApp/linuxSrc/mcBoard_E-1608.cpp
+++ measCompApp/linuxSrc/mcBoard_E-1608.cpp
@@ -33,11 +33,12 @@ mcE_1608::mcE_1608(const char *address)
     }
 
     if ((deviceInfo_.device.sock = openDevice(inet_addr(inet_ntoa(deviceInfo_.device.Address.sin_addr)),
-			    deviceInfo_.device.connectCode)) < 0) {
+                                              deviceInfo_.device.connectCode)) < 0) {
         printf("Error opening socket\n");
         return;
     }
-
+    buildGainTableAIn_E1608(&deviceInfo_);
+    buildGainTableAOut_E1608(&deviceInfo_);
 }
 
 int mcE_1608::setConfig(int InfoType, int DevNum, int ConfigItem, int ConfigVal) {
@@ -63,41 +64,99 @@ int mcE_1608::setConfig(int InfoType, int DevNum, int ConfigItem, int ConfigVal)
 int mcE_1608::getIOStatus(short *Status, long *CurCount, long *CurIndex,int FunctionType)
 {
     // Needs to be implemented
-	  return NOERRORS;
+    return NOERRORS;
 }
 
+static int gainToRange(int Gain)
+{
+    switch (Gain) {
+      case BIP1VOLTS:
+        return BP_1V;
+      case BIP2VOLTS:
+        return BP_2V;
+      case BIP5VOLTS:
+        return BP_5V;
+      case BIP10VOLTS:
+        return BP_10V;
+      default:
+        printf("Unsupported Gain=%d\n", Gain);
+        return BADRANGE;
+    }
+}
 int mcE_1608::aIn(int Chan, int Gain, USHORT *DataValue)
 {
-	  if (!AIn_E1608(&deviceInfo_, Chan, Gain, DataValue)) {
-	     return BADBOARD;
-	  }
-	  return NOERRORS;
+    uint16_t value;
+    int range = gainToRange(Gain);
+    if (range == BADRANGE) {
+        printf("Unsupported Gain=%d\n", Gain);
+        return BADRANGE;
+    }
+    if (!AIn_E1608(&deviceInfo_, Chan, (uint8_t)range, &value)) {
+       return BADBOARD;
+    }
+    *DataValue = value;
+    return NOERRORS;
+}
+
+int mcE_1608::aInScan(int LowChan, int HighChan, long Count, long *Rate, 
+                      int Gain, HGLOBAL MemHandle, int Options)
+{
+    uint8_t options = 0x0;
+    // We assume that aLoadQueue has previously been called and deviceInfo_.queue[0] contains number of channels
+    // being scanned.
+    double frequency = *Rate * deviceInfo_.queue[0];
+printf("mcE_1608::aInScan Count=%ld, frequency=%f, Options=0x%x\n", Count, frequency, Options);
+    if (!AInScanStart_E1608(&deviceInfo_, (uint32_t)Count, frequency, options)) {
+        printf("Error calling AInStartScan_E1608\n");
+        return BADBOARD;
+    }
+    return NOERRORS;
+}
+
+int mcE_1608::aLoadQueue(short *ChanArray, short *GainArray, int NumChans)
+{
+    deviceInfo_.queue[0] = NumChans;
+    int range;
+    for (int i=0; i<NumChans; i++) {
+        deviceInfo_.queue[2*i+1] = ChanArray[i];
+        range = gainToRange(GainArray[i]);
+        if (range == BADRANGE) {
+            printf("Unsupported Gain=%d\n", GainArray[i]);
+            return BADRANGE;
+        }
+        deviceInfo_.queue[2*i+2] = (uint8_t)range;    
+    }
+    if (!AInQueueW_E1608(&deviceInfo_)) {
+        printf("Error calling AInQueueW_E1608\n");
+        return BADBOARD;
+    }
+    return NOERRORS;
 }
 
 int mcE_1608::aOut(int Chan, int Gain, USHORT DataValue)
 {
-	  if (!AOut_E1608(&deviceInfo_, Chan, DataValue)) {
-	     return BADBOARD;
-	  }
-	  return NOERRORS;
+      if (!AOut_E1608(&deviceInfo_, Chan, DataValue)) {
+         return BADBOARD;
+      }
+      return NOERRORS;
 }
 
 int mcE_1608::cIn32(int CounterNum, ULONG *Count)
 {
     uint32_t counts;
-	  if (!CounterR_E1608(&deviceInfo_, &counts)) {
-	     return BADBOARD;
-	  }
-	  *Count = counts;
-	  return NOERRORS;
+      if (!CounterR_E1608(&deviceInfo_, &counts)) {
+         return BADBOARD;
+      }
+      *Count = counts;
+      return NOERRORS;
 }
 
 int mcE_1608::cLoad32(int RegNum, ULONG LoadValue)
 {
-	  if (!ResetCounter_E1608(&deviceInfo_)) {
-	     return BADBOARD;
-	  }
-	  return NOERRORS;
+      if (!ResetCounter_E1608(&deviceInfo_)) {
+         return BADBOARD;
+      }
+      return NOERRORS;
 }
 
 int mcE_1608::dBitOut(int PortType, int BitNum, USHORT BitValue)
