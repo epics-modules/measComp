7bcdcaa12bcd021b0d2e3d27caef1514e4a3463d
Mark Rivers <rivers@cars.uchicago.edu>
2018-03-19 14:58:11 -0500

    Finished aiScan implementation

--- measCompApp/linuxSrc/mcBoard_E-1608.h
+++ measCompApp/linuxSrc/mcBoard_E-1608.h
@@ -10,18 +10,29 @@
 class mcE_1608 : mcBoard {
 public:
     mcE_1608(const char *address);
+    // System functions
     int cbSetConfig(int InfoType, int DevNum, int ConfigItem, int ConfigVal);
     int cbGetIOStatus(short *Status, long *CurCount, long *CurIndex, int FunctionType);
+    int cbStopIOBackground(int FunctionType);
+
+    // Analog I/O functions
     int cbAIn(int Chan, int Gain, USHORT *DataValue);
     int cbAInScan(int LowChan, int HighChan, long Count, long *Rate, 
                 int Gain, HGLOBAL MemHandle, int Options);
     int cbALoadQueue(short *ChanArray, short *GainArray, int NumChans);
     int cbAOut(int Chan, int Gain, USHORT DataValue);
+
+    // Counter functions
     int cbCIn32(int CounterNum, ULONG *Count);
     int cbCLoad32(int RegNum, ULONG LoadValue);
+
+    // Digital I/O functions
     int cbDBitOut(int PortType, int BitNum, USHORT BitValue);
     int cbDConfigBit(int PortType, int BitNum, int Direction);
     int cbDIn(int PortType, USHORT *DataValue);
+
+    // Trigger functions
+    virtual int cbSetTrigger(int TrigType, USHORT LowThreshold, USHORT HighThreshold);
 
     void readThread();
 
@@ -29,6 +40,7 @@ private:
     DeviceInfo_E1608 deviceInfo_;
     std::thread *pReadThread_;
     std::mutex *pReadMutex_;
+
     bool aiScanAcquiring_;
     bool aiScanIsScaled_;
     double *aiScanScaledBuffer_;
@@ -37,6 +49,8 @@ private:
     int aiScanCurrentPoint_;
     int aiScanCurrentIndex_;
     int aiScanNumChans_;
+    int aiScanTrigType_;
+    int aiScanTrigCount_;  // Note currently used.  Implement for ReTrigger capability.
 };
 
 #endif /* mcBoard_E_1608Include */
