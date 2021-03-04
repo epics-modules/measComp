5fe666e9f0902488cc42475db3be72aaca4139d4
Mark Rivers <rivers@cars.uchicago.edu>
2021-03-04 11:08:23 -0600

    New file

---
+++ iocBoot/iocUSBTEMP/st.cmd
@@ -0,0 +1,25 @@
+< envPaths
+
+## Register all support components
+dbLoadDatabase "../../dbd/measCompApp.dbd"
+measCompApp_registerRecordDeviceDriver pdbbase
+
+epicsEnvSet(INPUT_POINTS, "4096")
+epicsEnvSet(OUTPUT_POINTS, "4096")
+
+## Configure port driver
+# MultiFunctionConfig((portName,        # The name to give to this asyn port driver
+#                      boardNum,        # The number of this board assigned by the Measurement Computing Instacal program 
+#                      maxInputPoints,  # Maximum number of input points for waveform digitizer
+#                      maxOutputPoints) # Maximum number of output points for waveform generator
+MultiFunctionConfig("USB_TEMP_1", 0, $(INPUT_POINTS), $(OUTPUT_POINTS))
+
+#asynSetTraceMask USB_TEMP_1 -1 255
+
+dbLoadTemplate("USB_TEMP.substitutions", "P=USB_TEMP:")
+
+< ../save_restore.cmd
+
+iocInit
+
+create_monitor_set("auto_settings.req",30)
