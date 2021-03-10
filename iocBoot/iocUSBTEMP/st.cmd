d3756e431417ef1e770600f5ae391a826bd2be12
Mark Rivers <rivers@cars.uchicago.edu>
2021-03-10 08:51:56 -0600

    Add serial number

--- iocBoot/iocUSBTEMP/st.cmd
+++ iocBoot/iocUSBTEMP/st.cmd
@@ -9,10 +9,10 @@ epicsEnvSet(OUTPUT_POINTS, "4096")
 
 ## Configure port driver
 # MultiFunctionConfig((portName,        # The name to give to this asyn port driver
-#                      boardNum,        # The number of this board assigned by the Measurement Computing Instacal program 
+#                      uniqueID,        # For USB the serial number.  For Ethernet the MAC address or IP address. 
 #                      maxInputPoints,  # Maximum number of input points for waveform digitizer
 #                      maxOutputPoints) # Maximum number of output points for waveform generator
-MultiFunctionConfig("USB_TEMP_1", 0, $(INPUT_POINTS), $(OUTPUT_POINTS))
+MultiFunctionConfig("USB_TEMP_1", "01F6335A", $(INPUT_POINTS), $(OUTPUT_POINTS))
 
 #asynSetTraceMask USB_TEMP_1 -1 255
 
