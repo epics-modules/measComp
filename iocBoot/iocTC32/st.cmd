57cd6554b798d518d41efcbbd01c53903420e822
Mark Rivers <rivers@cars.uchicago.edu>
2021-03-16 18:06:56 -0500

    Use environment variables, changes for better .substitutions files

--- iocBoot/iocTC32/st.cmd
+++ iocBoot/iocTC32/st.cmd
@@ -4,15 +4,19 @@
 dbLoadDatabase "../../dbd/measCompApp.dbd"
 measCompApp_registerRecordDeviceDriver pdbbase
 
+epicsEnvSet("PREFIX",        "TC32:")
+epicsEnvSet("PORT",          "TC32_1")
+epicsEnvSet("UNIQUE_ID",     "10.54.160.20")
+
 # Configure port driver
 ## Configure port driver
 # MultiFunctionConfig((portName,        # The name to give to this asyn port driver
 #                      uniqueID,        # For USB the serial number.  For Ethernet the MAC address or IP address.
 #                      maxInputPoints,  # Maximum number of input points for waveform digitizer
 #                      maxOutputPoints) # Maximum number of output points for waveform generator
-MultiFunctionConfig("TC32_1", "10.54.160.20", 0, 0)
+MultiFunctionConfig("$(PORT)", "$(UNIQUE_ID)", 1, 1)
 
-dbLoadTemplate("$(MEASCOMP)/db/TC32.substitutions", "P=TC32:")
+dbLoadTemplate("$(MEASCOMP)/db/TC32.substitutions", "P=$(PREFIX),PORT=$(PORT)")
 
 asynSetTraceIOMask TC32_1 -1 2
 #asynSetTraceMask TC32_1 -1 255
@@ -21,4 +25,4 @@ asynSetTraceIOMask TC32_1 -1 2
 
 iocInit
 
-create_monitor_set("auto_settings.req",30,"P=TC32:")
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX)")
