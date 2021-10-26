2cf4b30022da91d9febda248bdaf7a8db8305621
Mark Rivers <rivers@cars.uchicago.edu>
2021-10-26 07:22:35 -0500

    New file for USB-3105

---
+++ iocBoot/iocUSB3105/st.cmd
@@ -0,0 +1,26 @@
+< envPaths
+
+## Register all support components
+dbLoadDatabase "$(MEASCOMP)/dbd/measCompApp.dbd"
+measCompApp_registerRecordDeviceDriver pdbbase
+
+epicsEnvSet("PREFIX",        "USB3105:")
+epicsEnvSet("PORT",          "USB3105_1")
+epicsEnvSet("UNIQUE_ID",     "1AAA83E")
+
+## Configure port driver
+# MultiFunctionConfig((portName,        # The name to give to this asyn port driver
+#                      uniqueID,        # For USB the serial number.  For Ethernet the MAC address or IP address.
+#                      maxInputPoints,  # Maximum number of input points for waveform digitizer
+#                      maxOutputPoints) # Maximum number of output points for waveform generator
+MultiFunctionConfig("$(PORT)", "$(UNIQUE_ID)", 0, 0)
+
+#asynSetTraceMask($(PORT), -1, ERROR|FLOW|DRIVER)
+
+dbLoadTemplate("$(MEASCOMP)/db/USB3105.substitutions", "P=$(PREFIX),PORT=$(PORT)")
+
+< ../save_restore.cmd
+
+iocInit
+
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX)")
