4157867df387d63bdfbba1908c2d80c2ddb87f44
Mark Rivers <rivers@cars.uchicago.edu>
2022-04-14 16:52:51 -0500

    Many improvements

--- measCompApp/src/test_USB_CTR.cpp
+++ measCompApp/src/test_USB_CTR.cpp
@@ -1,7 +1,11 @@
 /*
     Does a CInScan on a USB-CTR04/08 to test how fast it can run.
-    Program requires 2 arguments, the UnigueID of the device (serial number for USB, MAC address for Ethernet), and the scan rate in Hz.
     Runs on both Windows and Linux.
+    It reads all of the counters and the DIO.
+    Program requires 3 arguments:
+      1) The UnigueID of the device (serial number for USB, MAC address for Ethernet)
+      2) The scan rate in Hz
+      3) The sleep time in ms between reading the scan data
 */
 
 #include <stdio.h>
@@ -17,10 +21,10 @@
 
 #define MAX_DEV_COUNT  100
 #define MAX_LIBRARY_MESSAGE_LEN 256
-#define MAX_SCAN_POINTS 2048
-#define SCAN_BUFFER_SIZE (MAX_SCAN_POINTS * (MAX_COUNTERS + 1))
+#define MAX_SCAN_POINTS 1000
 #define MAX_COUNTERS 8
-#define SLEEP_TIME 0.1
+#define MAX_DAQ_INPUTS (MAX_COUNTERS+1)
+#define SCAN_BUFFER_SIZE (MAX_SCAN_POINTS * MAX_DAQ_INPUTS)
 
 static void reportError(int err, const char *message)
 {
@@ -49,21 +53,23 @@ int main(int argc, char *argv[])
   int i;
   int err;
 
-  if (argc != 3) {
-    reportError(-1, "Must specify uniqueID as first argument");
+  if (argc != 4) {
+    reportError(-1, "Syntax: test_USB_CTR uniqueID rate sleepTime");
   }
   char *uniqueID = argv[1];
   double rate = atof(argv[2]);
+  int sleepTimeMs = atoi(argv[3]);
+
+  double *pCountsF64 = (double *) calloc(SCAN_BUFFER_SIZE, sizeof(double));
 
   // Get descriptors for all of the available DAQ devices
   #ifdef _WIN32
     int boardNum;
     cbIgnoreInstaCal();
-    int *pCountsI32 = (int *) calloc(SCAN_BUFFER_SIZE, sizeof(int));
     err = cbGetDaqDeviceInventory(ANY_IFC, devDescriptors, &numDevs);
+    int *pCountsI32 = (int *) pCountsF64;
   #else
     DaqDeviceHandle devHandle=-1;
-    double *pCountsF64 = (double *) calloc(SCAN_BUFFER_SIZE, sizeof(double));
     err = ulGetDaqDeviceInventory(ANY_IFC, devDescriptors, (unsigned int*)&numDevs);
   #endif
   reportError(err, "GetDaqDeviceInventory");
@@ -124,12 +130,12 @@ int main(int argc, char *argv[])
 					                CED_RISING_EDGE, CTS_TICK_20PT83ns, CDM_NONE, CDT_DEBOUNCE_0ns, CF_DEFAULT);
     #endif
   }
-  reportError(err, "ConfigScan");
+  reportError(err, "CConfigScan");
 
   #ifdef _WIN32
     long count;
     int chanCount;
-    #define MAX_DAQ_LEN 2*(MAX_COUNTERS+1)
+    #define MAX_DAQ_LEN (2*MAX_DAQ_INPUTS)
     short chanArray[MAX_DAQ_LEN];
     short chanTypeArray[MAX_DAQ_LEN];
     short gainArray[MAX_DAQ_LEN];
@@ -157,7 +163,7 @@ int main(int argc, char *argv[])
   #else
     ScanOption options = SO_DEFAULTIO;
     DaqInScanFlag flags = DAQINSCAN_FF_DEFAULT;
-    DaqInChanDescriptor *pDICD = new DaqInChanDescriptor[MAX_COUNTERS];
+    DaqInChanDescriptor *pDICD = new DaqInChanDescriptor[MAX_DAQ_INPUTS];
     int outChan=0;
     for (i=0; i<numCounters; i++) {
       pDICD[outChan].channel = i;
@@ -175,24 +181,39 @@ int main(int argc, char *argv[])
   #endif  
   reportError(err, "DaqInScan");
   
-  short ctrStatus=0;
+  short ctrStatus=1;
   long ctrCount, ctrIndex;
   while (ctrStatus) {
      #ifdef _WIN32
+      Sleep((int)(sleepTimeMs));
       err = cbGetIOStatus(boardNum, &ctrStatus, &ctrCount, &ctrIndex, DAQIFUNCTION);
+      ctrCount /= 2;
+      ctrIndex /= 2;
     #else
       ScanStatus scanStatus;
       TransferStatus xferStatus;
+      usleep((int)(sleepTimeMs * 1000));
       err = ulDaqInScanStatus(devHandle, &scanStatus, &xferStatus);
       ctrStatus = scanStatus;
       ctrCount = xferStatus.currentTotalCount;
       ctrIndex = xferStatus.currentIndex;
     #endif
     reportError(err, "ScanStatus");
-    printf("ctrStatus=%d, ctrCount=%ld, ctrIndex=%ld\n", ctrStatus, ctrCount, ctrIndex);
+    printf("ctrStatus=%d, ctrCount=%ld, ctrIndex=%ld, counts=", ctrStatus, ctrCount, ctrIndex);
+    #ifdef _WIN32
+      for (i=0; i<numCounters; i++) printf("%d ", pCountsI32[i]);
+      printf("0x%x\n", pCountsI32[i]);
+    #else
+      for (i=0; i<numCounters; i++) printf("%d ", (int)pCountsF64[ctrIndex+i]);
+      printf("0x%x\n", (unsigned int)pCountsF64[ctrIndex+i]);
+    #endif
   }
-  
-  // Print out the values
-
+  #ifdef _WIN32
+    err = cbStopBackground(boardNum, DAQIFUNCTION);
+  #else
+    err= ulDaqInScanStop(devHandle);
+  #endif
+  reportError(err, "Stop scan");
+  free(pCountsF64);
   return 0;
 }
