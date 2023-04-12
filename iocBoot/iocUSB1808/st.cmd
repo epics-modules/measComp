13c2109d99a4b53314bb5fadb733e666814a28d3
Mark Rivers <rivers@cars.uchicago.edu>
2023-04-12 10:56:21 -0500

    New file for USB-1808 and USB-1808X

---
+++ iocBoot/iocUSB1808/st.cmd
@@ -0,0 +1,34 @@
+< envPaths
+
+## Register all support components
+dbLoadDatabase "$(MEASCOMP)/dbd/measCompApp.dbd"
+measCompApp_registerRecordDeviceDriver pdbbase
+
+epicsEnvSet("PREFIX",        "USB1808:")
+epicsEnvSet("PORT",          "USB1808_1")
+epicsEnvSet("WDIG_POINTS",   "1048576")
+epicsEnvSet("WGEN_POINTS",   "1048576")
+epicsEnvSet("UNIQUE_ID",     "0215158E")
+
+## Configure port driver
+# MultiFunctionConfig((portName,        # The name to give to this asyn port driver
+#                      uniqueID,        # For USB the serial number.  For Ethernet the MAC address or IP address.
+#                      maxInputPoints,  # Maximum number of input points for waveform digitizer
+#                      maxOutputPoints) # Maximum number of output points for waveform generator
+MultiFunctionConfig("$(PORT)", "$(UNIQUE_ID)", $(WDIG_POINTS), $(WGEN_POINTS))
+
+#asynSetTraceMask($(PORT), -1, ERROR|FLOW|DRIVER)
+
+dbLoadTemplate("$(MEASCOMP)/db/USB1808.substitutions", "P=$(PREFIX),PORT=$(PORT),WDIG_POINTS=$(WDIG_POINTS),WGEN_POINTS=$(WGEN_POINTS)")
+
+< ../save_restore.cmd
+
+iocInit
+
+create_monitor_set("auto_settings.req",30,"P=$(PREFIX)")
+
+# Need to force the time arrays to process because the records are scan=I/O Intr
+# but asynPortDriver does not do array callbacks before iocInit.
+
+dbpf $(PREFIX)WaveDigDwell.PROC 1
+dbpf $(PREFIX)WaveGenUserDwell.PROC 1
